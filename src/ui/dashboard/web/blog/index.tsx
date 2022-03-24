03d009a417d3924457e271507a8f724d81cd8e86
qgatssdev <chivoomodu@gmail.com>
2022-03-25 00:09:14 +0100

    added searchbar and pagination

--- src/ui/dashboard/web/blog/index.tsx
+++ src/ui/dashboard/web/blog/index.tsx
@@ -1,21 +1,22 @@
-import Layout from "../layout";
-import BlogBanner from "./Banner";
-import BlogContent from "./Content";
-import BlogNavigation from "./Navigation";
+import Layout from '../layout';
+import BlogBanner from './Banner';
+import { useState } from 'react';
+import BlogContent from './Content';
 
-export default function WebBlog(){
-    return (
-        <Layout
-            externalStyles={["/styles/css/style.css"]}
-            navbar={"web"}
-            title="Church Blog"
-            withFooter={true}
-        >
-            <BlogBanner />
-            <div className="main_container">
-                <BlogContent />
-            </div>
-            <BlogNavigation />
-        </Layout>
-    );
+export default function WebBlog() {
+  const [searchTerm, setSearchTerm] = useState<string>('');
+
+  return (
+    <Layout
+      externalStyles={['/styles/css/style.css']}
+      navbar={'web'}
+      title="Church Blog"
+      withFooter={false}
+    >
+      <BlogBanner setSearchTerm={setSearchTerm} />
+      <div className="main_container">
+        <BlogContent searchTerm={searchTerm} />
+      </div>
+    </Layout>
+  );
 }
