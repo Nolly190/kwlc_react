5e0ab8bfa19e3cd68889505e5519fc1a66ad8091
Charles Onuorah <charles.onuorah@yahoo.com>
2022-04-26 22:41:07 +0100

    fix type safety issue

--- src/dto/Branch.dto.ts
+++ src/dto/Branch.dto.ts
@@ -1,73 +1,75 @@
 import { PastorsDetailsType, SliderType } from "../types/appTypes";
 
 export class BranchItemDTO {
-    public constructor(init?: Partial<BranchItemDTO>) {
-        Object.assign(this, init);
-    }
-    description: string;
-    timers: BranchServiceDTO[] = [];
-    title: string;
-    id: number;
-    image: BranchMediaDTO;
-    images: BranchMediaDTO[];
-    leadPastor: string;
-    favVerse: string;
-    location: string;
-    phoneNo: string[];
-    raised: number;
-    target: number;
+  public constructor(init?: Partial<BranchItemDTO>) {
+    Object.assign(this, init);
+  }
+  description: string;
+  timers: BranchServiceDTO[] = [];
+  title: string;
+  id: number;
+  image: BranchMediaDTO;
+  images: BranchMediaDTO[];
+  leadPastor: string;
+  favVerse: string;
+  location: string;
+  phoneNo: string[];
+  raised: number;
+  target: number;
+  city?: any;
 }
 
 export class BranchMediaDTO {
-    public constructor(init?: Partial<BranchMediaDTO>) {
-        Object.assign(this, init);
-    }
-    id: number;
-    title: string;
-    url: string;
-    description: string;
-    platform: string;
-    dateCreated: string;
-    createdBy: string;
+  public constructor(init?: Partial<BranchMediaDTO>) {
+    Object.assign(this, init);
+  }
+  id: number;
+  title: string;
+  url: string;
+  description: string;
+  platform: string;
+  dateCreated: string;
+  createdBy: string;
 }
 
 export class BranchServiceDTO {
-    public constructor(init?: Partial<BranchServiceDTO>) {
-        Object.assign(this, init);
-    }
-    id: number;
-    day: string;
-    time: string;
+  public constructor(init?: Partial<BranchServiceDTO>) {
+    Object.assign(this, init);
+  }
+  id: number;
+  day: string;
+  time: string;
 }
 
 export class BranchDTO {
-    public constructor(init?: Partial<BranchDTO>) {
-        Object.assign(this, init);
-    }
-    id: number;
-    name: string;
-    address: string;
-    contactNumber: string;
-    dateCreated: string;
-    location: string;
-    state: string;
-    isBranchHq: boolean;
-    eventVm: string[];
-    donationVm: string[];
-    liveStreamVm: string[];
-    mediaVm: BranchMediaDTO[];
-    messageVm: PastorsDetailsType;
-    sliderVm: SliderType[];
-    services: BranchServiceDTO[];
+  public constructor(init?: Partial<BranchDTO>) {
+    Object.assign(this, init);
+  }
+  id: number;
+  name: string;
+  address: string;
+  contactNumber: string;
+  dateCreated: string;
+  location: string;
+  state: string;
+  isBranchHq: boolean;
+  eventVm: string[];
+  donationVm: string[];
+  liveStreamVm: string[];
+  mediaVm: BranchMediaDTO[];
+  messageVm: PastorsDetailsType;
+  sliderVm: SliderType[];
+  services: BranchServiceDTO[];
+  city?: any;
 }
 
 export class BranchAssignAdminDTO {
-    public constructor(init?: Partial<BranchAssignAdminDTO>) {
-        Object.assign(this, init);
-    }
-    id: number;
-    userId: number;
-    branchId: number;
-    userType: string;
-    isResidential: boolean;
+  public constructor(init?: Partial<BranchAssignAdminDTO>) {
+    Object.assign(this, init);
+  }
+  id: number;
+  userId: number;
+  branchId: number;
+  userType: string;
+  isResidential: boolean;
 }
